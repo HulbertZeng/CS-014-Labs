ce317038d2ee21f01f08c17aa23d6759148c2784
HulbertZeng <54218666+HulbertZeng@users.noreply.github.com>
2019-10-02 18:20:08 -0700

    Update Lab 1: Linked List Playlist.h

--- Lab 1: Linked List Playlist.h
+++ Lab 1: Linked List Playlist.h
@@ -1,5 +1,21 @@
 
 
 class Playlist {
-
+  Default constructor
+  Parameterized constructor
+  public:
+  InsertAfter();
+  SetNext(); //Mutator
+  GetID(); //Accessor
+  GetSongName(); //Accessor
+  GetArtistName(); //Accessor
+  GetSongLength(); //Accessor
+  GetNext(); //Accessor
+  PrintPlaylistNode();
+  private:
+  string uniqueID; //Initialized to "none" in default constructor
+  string songName; //Initialized to "none" in default constructor
+  string artistName; //Initialized to "none" in default constructor
+  int songLength; //Initialized to 0 in default constructor
+  PlaylistNode* nextNodePtr; //Initialized to 0 in default constructor
 };
