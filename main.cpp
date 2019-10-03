d6bde2d786e28b44ec15cd9fdeb32ef2b08d5f1e
HulbertZeng <54218666+HulbertZeng@users.noreply.github.com>
2019-10-02 19:04:52 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,17 @@ using namespace std;
 
 #include "Playlist.h"
 
+void printMenu(string playlistTitle) {
+  
+}
+
 int main() {
-
-
-return 0;
+  string playlistTitle;
+  
+  cout << Enter playlist's title: " << endl;
+  cin >> playlistTitle;
+  
+  printMenu(playlistTitle);
+  
+  return 0;
 }
