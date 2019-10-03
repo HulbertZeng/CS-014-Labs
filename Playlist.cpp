f3e16e4aeb0572440c4f67d2f7d686bfaebdc6e3
HulbertZeng <54218666+HulbertZeng@users.noreply.github.com>
2019-10-02 18:43:40 -0700

    Update Playlist.cpp

--- Playlist.cpp
+++ Playlist.cpp
@@ -10,3 +10,20 @@ Playlist::Playlist() {
   songLength = 0;
   nextNodePtr = 0;
 }
+
+void Playlist::InsertAfter() {
+  cout << "FUNCTION InsertAfter() NOT COMPLETE" << endl;
+}
+
+void Playlist::SetNext() {
+  cout << "FUNCTION SetNext() NOT COMPLETE" << endl;
+}
+
+string Playlist::GetID() {
+  return uniqueID;
+}
+
+string Playlist::GetSongName() {
+  return songName;
+}
+
