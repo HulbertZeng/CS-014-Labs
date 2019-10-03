4de7a40445949d9e0fef528a9e3e9a53b5300a21
HulbertZeng <54218666+HulbertZeng@users.noreply.github.com>
2019-10-02 19:09:09 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,19 @@ using namespace std;
 #include "Playlist.h"
 
 void printMenu(string playlistTitle) {
+  char userInput;
   
+  while(userInput != 'q') {
+    cout << "JAMZ PLAYLIST MENU" << endl;
+    cout << "a - Add song" << endl;
+    cout << "d - Remove song" << endl;
+    cout << "c - Change position of song" << endl;
+    cout << "s - Output songs by specific artist" << endl;
+    cout << "t - Output total time of playlist (in seconds)" << endl;
+    cout << "o - Output full playlist" << endl;
+    cout << "q - Quit" << endl << endl;
+    cout << "Choose an option:" << endl;
+  }
 }
 
 int main() {
