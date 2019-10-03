fee498cde76f38e5ee160c6f5c79e1ed48df4d69
HulbertZeng <54218666+HulbertZeng@users.noreply.github.com>
2019-10-02 18:51:26 -0700

    Update Playlist.h

--- Playlist.h
+++ Playlist.h
@@ -2,17 +2,17 @@
 #define PLAYLIST_H
 
 class Playlist {
+  public:
   Playlist();
   Playlist(int);
-  public:
-  InsertAfter();
-  SetNext(); //Mutator
-  GetID(); //Accessor
-  GetSongName(); //Accessor
-  GetArtistName(); //Accessor
-  GetSongLength(); //Accessor
-  GetNext(); //Accessor
-  PrintPlaylistNode();
+  void InsertAfter();
+  void SetNext(); //Mutator
+  string GetID(); //Accessor
+  string GetSongName(); //Accessor
+  string GetArtistName(); //Accessor
+  int GetSongLength(); //Accessor
+  int GetNext(); //Accessor
+  void PrintPlaylistNode();
   private:
   string uniqueID; //Initialized to "none" in default constructor
   string songName; //Initialized to "none" in default constructor
