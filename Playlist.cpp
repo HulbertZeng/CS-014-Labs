78cff0245b66f64643f8509d4ceb89bc99b6620f
HulbertZeng <54218666+HulbertZeng@users.noreply.github.com>
2019-10-08 21:12:24 -0700

    Update Playlist.cpp

--- Playlist.cpp
+++ Playlist.cpp
@@ -1,54 +1,68 @@
+#include <cstddef>
 #include <iostream>
 using namespace std;
 
 #include "Playlist.h"
 
-PlaylistNode::Playlist() {
-  uniqueID = "none";
-  songName = "none";
-  artistName = "none";
-  songLength = 0;
-  nextNodePtr = 0;
+PlaylistNode::PlaylistNode() {
+    uniqueID = "none";
+    songName = "none";
+    artistName = "none";
+    songLength = 0;
+    this->nextNodePtr = nullptr;
 }
 
-PlaylistNode::Playlist(string id, string song, string artist, int length) {
-  uniqueID = id;
-  songName = song;
-  artistName = artist;
-  songLength = length;
-  this->nextNodePtr = 0;
+PlaylistNode::PlaylistNode(string id, string song, string artist, int length) {
+    uniqueID = id;
+    songName = song;
+    artistName = artist;
+    songLength = length;
+    this->nextNodePtr = nullptr;
 }
 
-void PlaylistNode::InsertAfter(PlaylistNode* addressSetAfter) {
-  this->nextNodePtr = addressSetAfter;
-  cout << "FUNCTION InsertAfter() NOT COMPLETE" << endl;
+void PlaylistNode::InsertAfter(PlaylistNode* &nextNode) {
+    if(this == nullptr) {
+        PlaylistNode* sub = nullptr;
+        sub = this;
+        sub = nextNode;
+    } else {
+        PlaylistNode* currNode = nullptr;
+        currNode = this;
+
+        while(currNode->GetNext() != nullptr) {
+            currNode = currNode->GetNext();
+        }
+        currNode->SetNext(nextNode);
+    }
 }
 
-void PlaylistNode::SetNext(PlaylistNode* toBeNext) {
-  this->nextNodePtr = toBeNext;
+void PlaylistNode::SetNext(PlaylistNode* &toBeNext) {
+    nextNodePtr = toBeNext;
 }
 
 string PlaylistNode::GetID() const{
-  return uniqueID;
+    return uniqueID;
 }
 
 string PlaylistNode::GetSongName() const{
-  return songName;
+    return songName;
 }
 
 string PlaylistNode::GetArtistName() const{
-  return artistName;
+    return artistName;
 }
 
 int PlaylistNode::GetSongLength() const{
-  return songLength;
+    return songLength;
 }
 
 PlaylistNode* PlaylistNode::GetNext() const{
-  return this->nextNodePtr;
+    return this->nextNodePtr;
 }
 
 void PlaylistNode::PrintPlaylistNode() {
-  cout << "FUNCTION PrintPlaylistNode() NOT COMPLETE" << endl;
+    cout << "Unique ID: " << GetID() << endl;
+    cout << "Song Name: " << GetSongName() << endl;
+    cout << "Artist Name: " << GetArtistName() << endl;
+    cout << "Song Length (in seconds): " << GetSongLength() << endl;
 }
-
